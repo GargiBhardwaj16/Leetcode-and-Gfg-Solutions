2a2d804458b92e98f7e1c54542a34ce8e73002ec
Gargi Bhardwaj <102040886+GargiBhardwaj16@users.noreply.github.com>
2024-04-03 21:58:55 +0530

    Time: 2 ms (59.73%), Space: 40.2 MB (69.83%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,73 @@
+class Solution {
+    public int totalNQueens(int n) {
+        char [][] board = new char[n][n];
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board.length; j++) {
+                board[i][j] = '.';
+            }
+        }
+        
+        List<List<String>> ans = new ArrayList<>();
+        queen(board, 0, ans);
+        return ans.size();
+    }
+
+    static void queen(char[][] board, int row, List<List<String>> list) {
+
+        if(row == board.length) {
+
+            list.add(construct(board));
+            return;
+        }
+
+        for (int col = 0; col < board.length; col++) {
+            if(isSafe(board, row, col)) {
+                board[row][col] = 'Q';
+                queen(board, row + 1, list);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    static List<String> construct(char[][] board) {
+
+        List<String> internal = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            String row = new String(board[i]);
+            internal.add(row);
+        }
+        return internal;
+    }
+
+    static boolean isSafe(char[][] board, int row, int col) {
+
+        //for checking vertical row
+        for (int i = row-1; i>=0; i--) {
+            if(board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        //for checking left diagonal
+
+        //int maxLeft = Math.min(row, col);
+
+        for (int i = row-1,j=col-1; i >=0 && j>=0; i--,j--) {
+            if(board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        //for checking right diagonal
+
+        int maxRight = Math.min(row, board.length - 1 - col);
+
+        for (int i = row-1,j=col+1; i >=0 && j<board.length; i--,j++) {
+            if(board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
