6338306a7edc5aa48aba96537a00f65bca31cffa
Gargi Bhardwaj <102040886+GargiBhardwaj16@users.noreply.github.com>
2024-04-06 14:50:37 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (9.68%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,11 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-         int prepre = 0, pre = 1;
-        for (int i = 0; i < n; i ++) {
-            int tmp = prepre + pre;
-            prepre = pre;
-            pre = tmp;
-        }
-        return pre;
+        if(n<=0) return 1;
+        if(n==1) return 1;
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        solve(n,dp); 
+        return dp[n];
+    }
+    public int solve(int n,int[]dp){
+        if(n<=0) return 1;
+        if(n==1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
     }
 }
