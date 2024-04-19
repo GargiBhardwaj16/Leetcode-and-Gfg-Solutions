dfb1aff13ed05880b6f7e0e9b7b44183bba08154
Gargi Bhardwaj <102040886+GargiBhardwaj16@users.noreply.github.com>
2024-04-19 21:56:07 +0530

    Time: 1 ms (100.00%), Space: 49.6 MB (34.95%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,34 @@
+class Solution{
+int rows , cols, totalIsland = 0;
+
+    public int numIslands(char[][] grid) {
+        rows = grid.length;
+        cols = grid[0].length;
+        if(rows == 0 || grid == null) return 0;
+        
+        for(int row = 0; row < rows ; row++) {
+            check(grid, row);
+        }
+        return totalIsland;
+    }
+
+    private void check(char[][] grid, int row) {
+        char[] currRow = grid[row];
+        for(int col = 0 ; col < cols; col++) {
+            if(currRow[col] == '1') {
+                totalIsland++;
+                bfs(grid, row, col);
+            }
+        }
+    }
+
+
+    private void bfs(char[][] grid, int row, int col) {
+        grid[row][col] = '0'; //visited
+        
+        if(row > 0 && grid[row - 1][col] == '1') bfs(grid, row - 1, col);
+        if(row + 1 < rows && grid[row + 1][col] == '1') bfs(grid, row + 1, col);
+        if(col > 0 && grid[row][col - 1] == '1') bfs(grid, row, col - 1);
+        if(col + 1 < cols && grid[row][col + 1] == '1') bfs(grid, row, col + 1);
+    }
+}
