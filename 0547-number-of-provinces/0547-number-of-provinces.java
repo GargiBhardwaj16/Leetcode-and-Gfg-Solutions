764a699cf8edce76da199ac4944112a44c3d93e2
Gargi Bhardwaj <102040886+GargiBhardwaj16@users.noreply.github.com>
2024-05-29 10:39:33 +0530

    Time: 3 ms (37.56%), Space: 46.5 MB (87.44%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,39 +1,34 @@
 class Solution {
+    public static void dfs(int node,boolean vis[],ArrayList<ArrayList<Integer>> adj){
+        vis[node]=true;
+        for(int i: adj.get(node)){
+            if(!vis[i]){
+                dfs(i,vis,adj);
+            }
+        }
+    }
     public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        boolean visited[] = new boolean[n];  // for storing that if a particular city is visited or not 
-
-        List<List<Integer>> graph = new ArrayList<>();
-
-        for(int i=0;i<n;i++)graph.add(new ArrayList<>());
-
-        for(int i=0;i<n;i++){  // make graph
-            for(int j=0;j<n;j++){
-                if(i==j)continue;
-                if(isConnected[i][j]==1){
-                    graph.get(i).add(j);
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        int v=isConnected.length;
+        for(int i=0;i<v;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<v;i++){
+            for(int j=0;j<v;j++){
+                if(isConnected[i][j]==1 && i!=j){
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
                 }
             }
         }
-
-        int totalProvinces = 0; // variable for storing number of components 
-        for(int i=0;i<n;i++){
-            if(visited[i]==false){ // if any city is not visited than we apply dfs on that city  
-                totalProvinces++;
-                dfs(graph,visited,i);
+        boolean vis[]=new boolean[v];
+        int cnt=0;
+        for(int i=0;i<v;i++){
+            if(!vis[i]){
+                cnt++;
+                dfs(i,vis,adj);
             }
         }
-        return totalProvinces;
-
-    }
-    public static void dfs(List<List<Integer>> graph,boolean visited[],int currentCity){
-        visited[currentCity]=true; // mark true because we visit that city
-        
-        for(int padosiCity:graph.get(currentCity)){  // now get the padosi city (if connected )of current city from graph and if that city is not visited than apply dfs on that city
-            if(visited[padosiCity]==false){
-                dfs(graph,visited,padosiCity);
-            }
-        }
-
+        return cnt;
     }
 }
