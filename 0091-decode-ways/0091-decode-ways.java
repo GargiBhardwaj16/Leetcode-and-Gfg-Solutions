5a0161d1493d43c1355ce4e298809ceaf756a4b0
Gargi Bhardwaj <102040886+GargiBhardwaj16@users.noreply.github.com>
2024-05-30 18:47:26 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (66.95%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n=s.length();
+        int dp[]=new int[n+1];
+        dp[0]=1;
+        if(s.charAt(0)!='0'){
+            dp[1]=1;
+        }
+        else{
+            return 0;
+        }
+        for(int i=2;i<=n;i++){
+            if(s.charAt(i-1)!='0'){
+                dp[i]=dp[i]+dp[i-1];
+            }
+            if(s.charAt(i-2)=='1'||(s.charAt(i-2)=='2'&& s.charAt(i-1)<='6')){
+                dp[i]=dp[i]+dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+}
