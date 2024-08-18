e99084cae6c460c279b037845be12725e60eee37
Gargi Bhardwaj <102040886+GargiBhardwaj16@users.noreply.github.com>
2024-08-18 22:16:06 +0530

    Time: 2 ms (91.97%), Space: 41.4 MB (65.31%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -1,21 +1,29 @@
 class Solution {
-
     public int nthUglyNumber(int n) {
-        TreeSet<Long> uglyNumbersSet = new TreeSet<>(); // TreeSet to store potential ugly numbers
-        uglyNumbersSet.add(1L); // Start with 1, the first ugly number
-        // TreeSet automatically sorts elements in ascending order and does not
-        // allow duplicate entries, just like a HashSet in python
-
-        Long currentUgly = 1L;
-        for (int i = 0; i < n; i++) {
-            currentUgly = uglyNumbersSet.pollFirst(); // Get the smallest number from the set and remove it
-
-            // Insert the next potential ugly numbers into the set
-            uglyNumbersSet.add(currentUgly * 2);
-            uglyNumbersSet.add(currentUgly * 3);
-            uglyNumbersSet.add(currentUgly * 5);
+        /*if(n<=6) return n;
+        int count=2;
+        for(int i=2;i<=5;i+=i-1){
+        for(int j=2;j<=n;j++){
+            if(j%i==0){
+                count++;
+            }
         }
-
-        return currentUgly.intValue(); // Return the nth ugly number
     }
+    return count;*/
+    int[] ugly = new int[n];
+        ugly[0] = 1;
+        int index2 = 0, index3 = 0, index5 = 0;
+        int factor2 = 2, factor3 = 3, factor5 = 5;
+        for(int i=1;i<n;i++){
+            int min = Math.min(Math.min(factor2,factor3),factor5);
+            ugly[i] = min;
+            if(factor2 == min)
+                factor2 = 2*ugly[++index2];
+            if(factor3 == min)
+                factor3 = 3*ugly[++index3];
+            if(factor5 == min)
+                factor5 = 5*ugly[++index5];
+        }
+        return ugly[n-1];
+  }
 }
